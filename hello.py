05ff6951be00c7b501f58a13af457d5b2ee27a33
chandu9188 <chanduinparimi@gmail.com>
2026-06-15 14:07:18 +0530

    Update hello.py
    Added projects

--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
 print("hello everybody...")
 
-print("="*78)
 print("this is sai Chandra vadhan")
+
 print("="*78)
 print("my info")
-
+print("="*78)
 
 print("expert in python")
 print("worked with transformers architecture")
 print("completed spring boot and jdbc")
+
+print("="*78)
+print("my projects")
+print("="*78)
+
+print("build autonomous ai agent using langchain and numpy")
+print("build compiler in assembly")
